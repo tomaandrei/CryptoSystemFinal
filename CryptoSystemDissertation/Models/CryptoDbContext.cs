18f6a0dd8e9227cb07eb02a5f267cb84f666f540
tomaandrei <tomaandreiandrei@gmail.com>
2016-07-18 19:00:03 +0300

    Web cfg and db

--- CryptoSystemDissertation/Models/CryptoDbContext.cs
+++ CryptoSystemDissertation/Models/CryptoDbContext.cs
@@ -4,6 +4,18 @@ namespace CryptoSystemDissertation.Models
 {
     public class CryptoDbContext : DbContext
     {
+        public CryptoDbContext() : base("CryptoDbUtcn")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<CryptoDbContext>(new CreateDatabaseIfNotExists<CryptoDbContext>());
+            modelBuilder.Entity<UserAccount>().ToTable("UserAccounts");
+            modelBuilder.Entity<ImageDetails>().ToTable("ImageDetails");
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<UserAccount> UserAccount { get; set; }
         public DbSet<ImageDetails> ImageDetails { get; set; }
     }
